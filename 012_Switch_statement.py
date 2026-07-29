ebdc9a5f8377dd56df3a988090c9ea5d2cbb3e0e
Urvashi-godara <urvashigodara95@gmail.com>
2026-07-29 01:01:41 -0500

    Implement switch statement using match in Python

---
+++ 012_Switch_statement.py
@@ -0,0 +1,32 @@
+n = int(input())
+
+match n:
+    case one if n==1:
+        print("One")
+        
+    case two if n==2:
+        print("Two")
+        
+    case three if n==3:
+        print("Three")
+        
+    case four if n==4:
+        print("Four")
+        
+    case five if n==5:
+        print("Five")
+        
+    case six if n==6:
+        print("Six")
+        
+    case seven if n==7:
+        print("Seven")
+        
+    case eight if n==8:
+        print("Eight")
+        
+    case nine if n==9:
+        print("Nine")
+        
+    case default:
+            print("Unknown")
